76adbf72886333d29e00f91737e38ee25b844265
Einar Kvam Lundberg <onlineeinar@gmail.com>
2025-04-08 10:56:34 +0200

    Implemented solver

--- Kubing/Trainers/HTRTrainer/script.js
+++ Kubing/Trainers/HTRTrainer/script.js
@@ -3,19 +3,39 @@ $(() => {
     keyActions();
 });
 
+let next = true;
+
 function nextScramble() {
     let r = Math.floor(Math.random() * 5);
     let scr = getSubsetScramble(["U2", "D2", "R2", "L2", "F2", "B2"], 15 + r);
-    let optimal = "?"; // Implement later
     $("#scramble").text(scr);
-    $("#optimal").text("(Optimal: " + optimal + ")");
+    $("#optimal").text("(Optimal: ?)");
     $("#drwscr").attr("scramble", scr);
+    getOptimal(scr);
 }
 
 function keyActions() {
     $("html").on('keydown', function (e) {
-        if (e.altKey && e.keyCode === 39) {
+        if (next && e.altKey && e.keyCode === 39) {
             nextScramble();
         }
     });
+}
+
+function toggleNext() {
+    next = !next;
+    $("#btnNext").prop("disabled", next ? "" : "disabled");
+}
+
+function getOptimal(scramble) {
+    toggleNext();
+    let w = new Worker("worker.js");
+    w.postMessage([scramble]);
+    w.onmessage = e => {
+        toggleNext();
+        optimal = e.data[0];
+        let optlen = optimal === "?" ? "> 10" : optimal.split(" ").length;
+        $("#optimal").text("(Optimal: " + optlen + ")");
+        $("#optimalSol").text(optimal);
+    }
 }

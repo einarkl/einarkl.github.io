34297b897113b40637f83e05932e9c4899e28391
Einar Kvam Lundberg <onlineeinar@gmail.com>
2026-01-03 18:49:37 +0100

    Revert "Added art sorting"
    This reverts commit f9ef48eb9df0f927b1e41c87ecda418cbb7e750e.

--- Pokemon/Jirachi/script.js
+++ Pokemon/Jirachi/script.js
@@ -2,7 +2,7 @@ const SHEET_ID = "1oC4KAdpbUDg64wjNZ_AU9SflkZsxBXcpCOSgLYwpO8Q";
 const URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:json`;
 
 let allItems = [];
-let currentLanguageOrder = "art";
+let currentLanguageOrder = "release-old";
 let currentCollectionOrder = "none";
 let currentLanguageFilter = "all";
 
@@ -53,8 +53,7 @@ fetch(URL)
         otherPokemon,
         embeddedPicture,
         pictureUrl,
-        language,
-        art
+        language
       ] = row.c.map(c => c?.v ?? "");
 
       return {
@@ -67,8 +66,7 @@ fetch(URL)
         type,
         rarity,
         pictureUrl,
-        language,
-        art: Number(art) || 0
+        language
       };
     });
 
@@ -120,14 +118,6 @@ function sortItems(items) {
     if (currentLanguageOrder === 'language-az') {
       const diff = String(a.language || '').localeCompare(String(b.language || ''));
       if (diff !== 0) return diff;
-    }
-
-    // Art id order
-    if (currentLanguageOrder === 'art') {
-      const diff = (a.art || 0) - (b.art || 0);
-      if (diff !== 0) return diff;
-      // If art id is equal, sort by release year old->new
-      return (a.releaseYear || 0) - (b.releaseYear || 0);
     }
 
     // Chronological order
